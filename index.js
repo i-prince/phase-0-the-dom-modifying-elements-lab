5a06f4bc1e4c6b3c631f26db720ea6394b9464f2
i-prince <molatonny@gmail.com>
2023-07-21 14:19:44 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+//Delete the <main> using its id selector
+const deleteMain = document.querySelector('#main');
+deleteMain.remove();
+
+//Create a new element 
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "is the champion";
